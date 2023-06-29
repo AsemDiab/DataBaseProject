01cddf01bb8dce6fdb2ff1bd74e896e014b5c4a2
osama <osama1111222@gmail.com>
2023-06-29 12:29:36 +0300

    Osama 29/6

--- DataBase-project/src/main/java/com/example/databaseproject/LibreanPageController.java
+++ DataBase-project/src/main/java/com/example/databaseproject/LibreanPageController.java
@@ -10,25 +10,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LibreanPageController implements Initializable {
 
-    private Parent root;
-    private Stage stage;
-    private Scene scene;
     /////////////////////////////////////////////
     //            switch scenes                //
     /////////////////////////////////////////////
     public void swetchToHome(ActionEvent e) throws IOException {
-        root= FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        stage=(Stage)((Node)e.getSource()).getScene().getWindow();
-        scene=new Scene(root);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HomePage.fxml")));
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setX(60);
         stage.setY(10);
         stage.setScene(scene);
         stage.show();
-    }
+    }//2
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
